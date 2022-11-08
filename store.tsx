25b9927846bd76c6b3b6b23c2133df8d2ce48bbd
jamesla <jamesgmccallum@gmail.com>
2022-11-08 12:05:10 +1000

    tidy up poc

--- store.tsx
+++ store.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 type ChildrenType = {
   children: React.ReactNode
 };
 
-export const StoreContext = React.createContext({
-  test: [''] 
-});
+export const StoreContext = React.createContext();
 
 export const StoreProvider = (({ children }: ChildrenType) => {
-  const [test] = React.useState(['']);
-  //const test =  "world"
+  const [test] = React.useState(['world']);
 
   const store = {
-    test: test,
+    test,
   };
 
   return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
